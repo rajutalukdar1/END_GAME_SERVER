76936e39a3452a5f64342b0f3ab9048e32167e6e
rajutalukdar1 <lalutaluker@gmail.com>
2022-12-28 23:00:26 +0600

    post and get

--- index.js
+++ index.js
@@ -15,7 +15,33 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(client);
 
+// main function 
+async function run() {
+    try {
+        const srjrPostCollection = client.db('srjr').collection('srjrPost');
 
+
+
+        app.post('/srjrPost', async (req, res) => {
+            const post = req.body;
+            // console.log(post);
+            const result = await srjrPostCollection.insertOne(post);
+            console.log(result);
+            res.send(result);
+        })
+
+        app.get('/allPosts', async (req, res) => {
+            const query = {}
+            const cursor = srjrPostCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error))
 
 
 
